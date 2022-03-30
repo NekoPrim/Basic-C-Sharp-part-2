8fcfddf29220f81f8a460a16ac43f0fc102e4a43
Desiree Quade <nekodez@gmail.com>
2022-03-30 18:11:01 -0500

    completed question 1

--- Program.cs
+++ Program.cs
@@ -6,40 +6,45 @@ Console.WriteLine("---- 1. For loops ----");
 // Example: a for loop to console.log numbers from 0 to 3
 Console.WriteLine("count from 0 to 3");
 // start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
-// for (let i=0; i<4; i++) {
-//   console.log(i);
-// }
+for (int i = 0; i < 4; i ++) 
+{
+    Console.WriteLine(i);
+}
 
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5
 //   - Which part of the example loop do you need to change to do this?
-// console.log('count from 0 to 5');
+Console.Write("count from 0 to 5");
 
-// for (i= 0; i < 6; i ++) {
-//   console.log(i);
-// }
+for (i = 0; i < 6; i ++) 
+{
+    Console.WriteLine(i);
+}
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
-// console.log('count from 3 to 5');
+Console.WriteLine("count from 3 to 5");
 
-// for (i= 3; i < 6; i ++) {
-//   console.log(i);
-// }
+for (i = 3; i < 6; i ++) 
+{
+    Console.WriteLine(i);
+}
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
-// console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+Console.WriteLine("count even numbers from 2 to 10 (2, 4, 6, 8, 10):");
 
-// for (i= 2; i < 11; i += 2) {
-//   console.log(i);
-// }
+for (i = 2; i < 11; i += 2) 
+{
+    Console.WriteLine(i);
+}
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a counddown from 5 to 0
-// console.log('STRETCH: countdown from 5 to 0');
+Console.WriteLine("STRETCH: countdown from 5 to 0");
 
-// for (i= 5; i >= 0; i --){
-//   console.log(i);
-// }
+for (i = 5; i >= 0; i --)
+{
+    Console.WriteLine(i);
+}
 
 // 2. For of loops
 // console.log('---- 2. For Of loop ----');
